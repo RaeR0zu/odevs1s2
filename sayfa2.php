cc931fd94e5dd101709c3b3082fa1d59c1e56c2e
Nur D <108955803+snurdogan@users.noreply.github.com>
2022-10-03 23:38:24 +0300

    php
    -

---
+++ sayfa2.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="tr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sayfa2 </title>
+   
+</head>
+<body>
+     <h1> 4 İşlem</h1>
+    <form name="islem">
+         Sayı 1: <input type="text" name="sayı1" placeholder="Bir Sayı Giriniz." required="required">  <br>
+         <br>
+        İŞLEMLER : 
+        <select name="işlemler" id="is">
+            <option>Toplama</option>
+            <option>Çıkarma</option>
+            <option>Çarpma</option>
+            <option>Bölme</option>
+            
+        </select>
+        <br>
+        <br>
+         Sayı 2:  <input type="text" name="sayı2" placeholder="Bir Sayı Giriniz:" required="required"> 
+    <br>
+    <br>
+    <div class="buttons">
+    
+        <button> Hesapla</button> 
+        
+
+    </div>
+    </form>
+</body>
+</html>
